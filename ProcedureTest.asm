400443c70dd3aa3f5f8b4339bfd580b76367d7d8
Harry <33969918+NoSharp@users.noreply.github.com>
2019-03-06 21:27:42 +0000

    More Fuctions
    Removed my dumb remark about setting a location 💯

--- ProcedureTest.asm
+++ ProcedureTest.asm
@@ -1,7 +1,8 @@
+
 section .data
     ; Variables.
 
-    msg db "Howdy.", 0xa ; Define our variable and assign it to a location.
+    msg db "Howdy.", 0xa ; "0xa" == New line. Define our variable
     msg_len  equ $-msg ; set the length of the output.
 
 section .text
@@ -12,14 +13,19 @@ section .text
 _start:
                
         call print
-        mov ebx, 0
-        mov eax, 1
-        int 80h
+        ; EXIT OUR CODE ;)
+        call exit
 
-print:
-    mov ecx, msg
-    mov edx, msg_len
-    mov ebx, 1
-    mov eax, 4
+exit:
+    mov ebx, 0
+    mov eax, 1
     int 80h
     ret
+
+print:
+    mov ecx, msg ; message to write.
+    mov edx, msg_len ; message length.
+    mov ebx, 1 ; print 
+    mov eax, 4 ; Sys_write call.
+    int 80h
+    ret ; return
